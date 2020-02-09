e1b16f286f523aa5bfda5acfc0ff4b0203ae4dae
Chris Longden <longden@longden.net>
2020-02-09 18:37:29 +0100

    Handle existing account

--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
-    s3: {
-      REGION: "eu-central-1",
-      BUCKET: "notes-app-uploads-cl"
-    },
-    apiGateway: {
-      REGION: "eu-central-1",
-      URL: "https://6u26xn64ub.execute-api.eu-central-1.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "eu-central-1",
-      USER_POOL_ID: "eu-central-1_FpxUDSX3U",
-      APP_CLIENT_ID: "5841laobkcujjki7m4guekhuem",
-      IDENTITY_POOL_ID: "eu-central-1:75455d4f-32c5-43fd-bd17-ac75f0df9936"
-    }
-  };
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "notes-app-uploads-cl"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://6u26xn64ub.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_FpxUDSX3U",
+    APP_CLIENT_ID: "5841laobkcujjki7m4guekhuem",
+    IDENTITY_POOL_ID: "eu-central-1:75455d4f-32c5-43fd-bd17-ac75f0df9936"
+  }
+};
