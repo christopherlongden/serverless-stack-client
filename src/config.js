269a344259252286bb1b0ef629fd4f9359460276
Chris Longden <longden@longden.net>
2020-02-10 17:52:03 +0100

    Updated with dev/production config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+// dev
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   STRIPE_KEY: "pk_test_UJQs7zJIzntPN1pXdLRBBIIq00tzxVN5PJ",
@@ -16,3 +17,24 @@ export default {
     IDENTITY_POOL_ID: "eu-central-1:602aebe3-423b-4f33-b8fd-c66d4979b0be"
   }
 };
+/*
+// Production
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_UJQs7zJIzntPN1pXdLRBBIIq00tzxVN5PJ",
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "notes-app-api-prod-attachmentsbucket-18f4bre50zw21"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://9fiwq44h75.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_RzWP6juIg",
+    APP_CLIENT_ID: "1tjujqp131kqv2lm0k0r65dkt6",
+    IDENTITY_POOL_ID: "eu-central-1:08ab8c9b-15ea-45ef-9e8c-e28461801629"
+  }
+};
+*/
